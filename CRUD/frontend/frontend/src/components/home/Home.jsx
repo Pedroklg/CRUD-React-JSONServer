38a15e795cfb3a9c9c7dc9362132ffb7ca9164c8
Pedroklg <162770884+Pedroklg@users.noreply.github.com>
2024-05-21 18:05:06 -0300

    Add files via upload

---
+++ CRUD/frontend/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from '../template/Main'
+
+export default props =>
+  <Main icon="home" title="Inicio"
+    subtitle="CRUD React.">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema para construcçao
+      de um cadastro desenvolvido em React!</p>
+  </Main>
---
+++ CRUD/frontend/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+import "font-awesome/css/font-awesome.min.css";
+
+export default props => 
+  <footer className="footer">
+    <span>
+      CRUD React desenvolvido por
+      <strong> Pedro G.</strong>
+    </span>
+  </footer>
